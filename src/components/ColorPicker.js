9d6300a72e017f3c02ed175a3e09898d58fdf097
roy <roy.yair@gmail.com>
2018-12-10 21:46:12 +0200

    replace the whole picker mechanism from grid of div's to canvas

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,19 +1,52 @@
 import React, { Component } from 'react';
 import './ColorPicker.css';
-import Pixel from './Pixel'
 import PickedColor from './PickedColor'
 
 class ColorPicker extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {
+    this.canvas = null;
+    this.state  = {
       pickedColor: { h: 0, s: 0, l: 0 }
     }
+  };
+
+  componentDidMount() {
+    this.updateCanvas();
   }
 
-  onSelect = (h, s, l) => {
-    console.log('onselect!');
+  updateCanvas = () => {
+    let c              = this.canvas.getContext("2d");
+    this.canvas.width  = 101;
+    this.canvas.height = 101;
+
+    let width  = this.canvas.width;
+    let height = this.canvas.height;
+
+    c.fillStyle = "rgb(0, 0, 0)";
+
+    c.fillRect(0, 0, width, height);
+
+    for (let x = height - 1; x >= 0; x--) {
+      for (let y = 0; y < width; y++) {
+        c.fillStyle = "hsl(" + 0 + ", " + x + "%, " + y + "%)";
+        c.fillRect(x, height - 1 - y, 1, 1);
+      }
+    }
+  };
+
+  onSelect = (e) => {
+    const posX = e.pageX - this.canvas.offsetLeft;
+    const posY = e.pageY - this.canvas.offsetTop;
+
+    // ignore when mouse clicks on border of canvas
+    if (posX < 0 || posY < 0) return;
+
+    const h = 0;
+    const s = posX;
+    const l = 100 - posY;
+
     this.setState(() => ({
       pickedColor: {
         h, s, l
@@ -21,39 +54,17 @@ class ColorPicker extends Component {
     }))
   };
 
-  createTable = () => {
-    let table = [];
-
-    for (let i = 100; i >= 0; i--) {
-      let children = [];
-
-      for (let j = 0; j < 101; j++) {
-        children.push(<Pixel key={`${i}${j}`}
-                             hue={0}
-                             saturation={j}
-                             lightness={i}
-                             onSelect={this.onSelect}
-                             className={'divTableCell'}/>)
-      }
-
-      table.push(<div key={i} className={'divTableRow'}>{children}</div>)
-    }
-
-    return table
-  };
-
-
   render() {
     const { pickedColor } = this.state;
 
     return (
-      <div class="container">
+      <div className="container">
         <div className="picker">
+
           <h1>Color Picker</h1>
-          <div className={'divTable'}>
-            <div className={'divTableBody'}>
-              {this.createTable()}
-            </div>
+          <div className="canvas-wrapper">
+            <canvas ref={(elem) => this.canvas = elem}
+                    onClick={(e) => this.onSelect(e)}/>
           </div>
 
           <PickedColor hue={pickedColor.h}
